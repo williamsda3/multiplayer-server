9b320bb2640cf6f8a0cc4530fa28dd3272b07567
williamsda3 <williamsda3@vcu.edu>
2023-11-25 20:04:30 -0500

    .....

--- server.py
+++ server.py
@@ -1,25 +1,51 @@
-import asyncio
-import websockets
+import socketio
+import eventlet
+from flask_cors import CORS
 
-async def game_handler(websocket, path):
-    print(f"New connection from {websocket.remote_address}")
+eventlet.monkey_patch()
 
-    try:
-        # Wait for the user's move
-        move = await websocket.recv()
-        print(f"Received move: {move}")
+sio = socketio.Server(cors_allowed_origins="*")
+app = socketio.WSGIApp(sio)
 
-        # Process the move (you can add your game logic here)
+# Use CORS
+CORS(app)
 
-        # Send the result back to the user
-        result = "Result Placeholder"  # Replace with your actual result
-        await websocket.send(result)
+# Dictionary to store moves from each player
+player_moves = {}
 
-    finally:
-        print(f"Connection with {websocket.remote_address} closed")
+@sio.event
+def connect(sid, environ):
+    print('Client connected:', sid)
 
-# Run the WebSocket server
-start_server = websockets.serve(game_handler, "0.0.0.0", 5001)
+@sio.event
+def disconnect(sid):
+    print('Client disconnected:', sid)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+@sio.event
+def send_move(sid, data):
+    print(f'Received move from client {sid}: {data}')
+
+    # Store the move for the player
+    player_moves[sid] = data
+
+    # Check if moves from both players are available
+    if len(player_moves) == 2:
+        # Determine the result based on the game logic (customize this part)
+        result = determine_game_result(player_moves)
+
+        # Emit the result to both players
+        for player_sid, move in player_moves.items():
+            sio.emit('show_result', {'move': move, 'result': result}, room=player_sid)
+
+        # Clear stored moves for the next round
+        player_moves.clear()
+
+def determine_game_result(moves):
+    # Customize this function based on your game logic
+    # For example, you can compare moves and return the result
+    # (e.g., 'player1_wins', 'player2_wins', 'draw')
+    # This is a placeholder logic; you need to adapt it to your game rules.
+    return 'result_placeholder'
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
